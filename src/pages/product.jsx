b1497dc06f9a661170a809d6831d838359a0fc66
zumbost1k <matusevich.mi@gmail.com>
2023-07-31 16:17:23 +0300

    reviews fixes

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import ProductPath from "../product_path/product_path";
-import ProductInfo from "../product_info/product_info";
-import Description from "../description/description";
-import Products from "../products/products";
+import React from 'react';
+import ProductPath from '@/product_path/product_path';
+import ProductInfo from '@/product_info/product_info';
+import ProductsDescriptions from '@/description/description';
+import Products from '@/products/products';
+
 
 const Product = () => {
     return (
         <div>
             <ProductPath />
             <ProductInfo />
-            <Description />
+            <ProductsDescriptions  />
             <Products title={'Related Products'} />
         </div>
     )
