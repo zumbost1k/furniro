9d6954d623f59ece69e518169cac0c4d0e6e491e
zumbost1k <matusevich.mi@gmail.com>
2023-08-10 14:48:12 +0300

    review fixes

--- src/contact_form/contact_form.jsx
+++ src/contact_form/contact_form.jsx
@@ -7,11 +7,13 @@ const contacts = [
         title: 'Address',
         text: '236 5th SE Avenue, New York NY10000, United States',
         additionalInfo: ''
+        additionalInfo: ''
     },
     {
         path: 'bxs_phone',
         title: 'Phone',
         text: 'Mobile: +(84) 546-6789 Hotline: +(84) 456-6789',
+        additionalInfo: ''
         additionalInfo: ''
     },
     {
@@ -19,37 +21,51 @@ const contacts = [
         title: 'Working Time',
         text: `Monday-Friday: 9:00 - 22:00 `,
         additionalInfo: 'Saturday-Sunday: 9:00 - 21:00'
+        additionalInfo: 'Saturday-Sunday: 9:00 - 21:00'
     },
 ]
 
 
 const contactForms = [
+const contactForms = [
     {
+        label: 'Your name',
         label: 'Your name',
         type: 'text',
         placeholder: 'Abc',
+        placeholder: 'Abc',
         name: 'name',
+        isBig: false
         isBig: false
     },
     {
         label: 'Email address',
+        label: 'Email address',
         type: 'email',
         placeholder: 'Abc@def.com',
+        placeholder: 'Abc@def.com',
         name: 'mail',
+        isBig: false
         isBig: false
     },
     {
         label: 'Subject',
+        label: 'Subject',
         type: 'text',
         placeholder: 'This is an optional',
+        placeholder: 'This is an optional',
         name: 'subj',
+        isBig: false
         isBig: false
     },
     {
         label: 'Message',
+        label: 'Message',
         type: 'text',
         placeholder: 'Hi! i’d like to ask about',
+        placeholder: 'Hi! i’d like to ask about',
         name: 'mess',
+        isBig: true
         isBig: true
     },
 ]
@@ -58,9 +74,11 @@ const contactList = contacts.map(contact => {
     return (
         <div className='contact_item_container'>
             <img width='30' height='30' src={`/photos/${contact.path}.svg`} alt={contact.title} />
+            <img width='30' height='30' src={`/photos/${contact.path}.svg`} alt={contact.title} />
             <div className='contact_item_text'>
                 <h3 className='contact_item_text_title'>{contact.title}</h3>
                 <p className='contact_item_text_text'>{contact.text}</p>
+                {contact.additionalInfo && <p className='contact_item_text_text'>{contact.additionalInfo}</p>}
                 {contact.additionalInfo && <p className='contact_item_text_text'>{contact.additionalInfo}</p>}
             </div>
         </div>
@@ -68,8 +86,12 @@ const contactList = contacts.map(contact => {
 })
 
 const placeholdersList = contactForms.map((placeholder, index) => {
+const placeholdersList = contactForms.map((placeholder, index) => {
     return (
         <div className='contact_form_item_inner'>
+            <label className='form_item_label' for={placeholder.name}>{placeholder.label} </label>
+            <input className={placeholder.isBig ? 'form_item_input big_form_input' : 'form_item_input'} type={placeholder.type} name='contact_form_item' id={placeholder.name} placeholder={placeholder.placeholder} required />
+        </div>
             <label className='form_item_label' for={placeholder.name}>{placeholder.label} </label>
             {placeholder.isBig ?
                 < textarea className='form_item_input big_form_input' name='contact_form_item' id={placeholder.name} placeholder={placeholder.placeholder} required />
