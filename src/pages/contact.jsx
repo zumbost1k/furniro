e91f594c84aa0b41728ec0ee8f61a8ba4439311b
zumbost1k <matusevich.mi@gmail.com>
2023-08-10 15:18:04 +0300

    add aliased imports

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ShopHeader from '../shop_header/shop_header';
-import DiscountShop from '../discount_shop/discount_shop';
-import ContactForm from '../contact_form/contact_form';
+import ShopHeader from '@/shop_header/shop_header';
+import DiscountShop from '@/discount_shop/discount_shop';
+import ContactForm from '@/contact_form/contact_form';
 
 const ContactPage = () => {
     return (
